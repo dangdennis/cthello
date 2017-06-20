dcdf648ed5325790f56652165494b90e72a84170
Dennis Dang <dangggdennis.com>
2017-06-20 10:31:21 -0700

    gamepieces + toggles

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
-function Game() {
-	var self = this
-	var gameSize = 8;
+var gameSize = 8;
 
-	self.initialize = function() {
-		$(document).ready(function() {
-			self.createBoard();
-		})
-	};
+//**************************//
+// ****    DOC READY   **** //
+//**************************//
 
-	self.createBoard = function() {
-		for(var i = 0; i < 8; i++){
-			var rowID = String.fromCharCode(65+i);
-			var row = $("<div>").addClass("row").attr("row" + rowID);
-			$("#gameboard").append(row);
-			for(var j = 0; j < 8; j++) {
-				var id = String.fromCharCode(65 + i);
-				var square = $("<div>").addClass("square").attr("id",id);
-			};
+$(document).ready(function(){
+	createBoard();
+	eventHandlers();
+})
+
+//**************************//
+// **** EVENT HANDLERS **** //
+//**************************//
+
+function eventHandlers() {
+	$(".square").on("click",flipWhite);
+}
+
+function flipWhite() {
+	$(this).toggleClass("whitePiece");
+}
+
+function blackWhite() {
+	$(this).toggleClass("blackPiece");
+}
+
+//**************************//
+// **** GAME FUNCTIONS **** //
+//**************************//
+
+function createBoard() {
+	for(var i = 0; i < gameSize; i++){
+		var rowID = String.fromCharCode(65+i);
+		var row = $("<div>").addClass("row").attr("id",rowID);
+		$(".main").append(row);
+		for(var j = 0; j < gameSize; j++) {
+			var idNum = "" + rowID + j;
+			var square = $("<div>").addClass("square").attr("id",idNum).appendTo(row);
 		};
 	};
+}
 
-	self.initialize();
-};
 
-var game = Game();
+
+// function generateCards(){
+// 	// One loop per row of cards //
+// 	for(var i=0;i<numberOfCards;i++) {
+// 		// ***** div.cardContainer -> div.card -> div.front + div.back -> img ***** //
+// 		var cardContainer = $("<div>").addClass("cardContainer").appendTo(".row");
+// 		var newCard = $("<div>").addClass("card").appendTo(cardContainer);
+// 		var newBack = $("<div>").addClass("back").appendTo(newCard);
+// 		var newBackImg = $("<img>").attr("id", obj.id).css('background-image', 'url(' + obj.url + ')').appendTo(newBack);
+// 		var newFront = $("<div>").addClass("front").appendTo(newCard);
+// 		var newFrontImg = $("<img>").attr("src", "pictures/legend_cardback.png").appendTo(newFront);
+// 	}
+// }
