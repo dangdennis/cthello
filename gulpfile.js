368f21dbb72100f951e686c14e050090c82c3e5a
Dennis Dang <dangggdennis@gmail.com>
2017-09-06 18:42:47 -0700

    responsive design implementation

--- gulpfile.js
+++ gulpfile.js
@@ -27,15 +27,3 @@ gulp.task("watch", ["browserSync", "sass"], function() {
 	gulp.watch("app/*.html", browserSync.reload);
 	gulp.watch("app/js/**/*.js", browserSync.reload);
 });
-
-/* 
-
-Gulp Task Template
-
-gulp.task('task-name', function () {
-  return gulp.src('source-files') // Get source files with gulp.src
-    .pipe(aGulpPlugin()) // Sends it through a gulp plugin
-    .pipe(gulp.dest('destination')) // Outputs the file in the destination folder
-}) 
-
-*/
